0fbacc70b973fcafb62434b3a789f1fd6b6faed9
Narsimhareddy28 <k.sainarsimhareddy03@gmail.com>
2023-12-07 13:46:48 -0600

    Updates

---
+++ precache-manifest.faebd9f9ddce13c27185f00daf057006.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24330a2c45f57d11e3a0",
+    "url": "/Reactportfolio/static/css/main.d012d02d.chunk.css"
+  },
+  {
+    "revision": "24330a2c45f57d11e3a0",
+    "url": "/Reactportfolio/static/js/main.24330a2c.chunk.js"
+  },
+  {
+    "revision": "8acf010aac7ee98b1108",
+    "url": "/Reactportfolio/static/js/1.8acf010a.chunk.js"
+  },
+  {
+    "revision": "ab5e72a456c3592742c7",
+    "url": "/Reactportfolio/static/js/runtime~main.ab5e72a4.js"
+  },
+  {
+    "revision": "63320d96b682253f73cd60cc85cb155c",
+    "url": "/Reactportfolio/index.html"
+  }
+];
