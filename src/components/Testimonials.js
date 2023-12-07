430816b10707967b9f827e2d8ece12b63e1b14dd
Narsimhareddy28 <k.sainarsimhareddy03@gmail.com>
2023-12-07 17:45:19 -0600

    commit

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -28,7 +28,7 @@ export default class Testimonials extends Component {
           <div className="row">
             <div className="two columns header-col">
               <h1>
-                <span>More About ME</span>
+                <span style={{color:"black", zIndex:"999"}}>More About ME</span>
               </h1>
             </div>
             <div className="ten columns flex-container">
--- src/resumeData.js
+++ src/resumeData.js
@@ -97,13 +97,22 @@ let resumeData = {
     ],
     "testimonials":[
       {
-        "description":"Video Games ",
-        "name":"Some technical guy"
+        "description":"More About Myself  ",
+        "name":"IT Club President: Led the IT Club, organizing tech events and managing projects to enhance members' IT skills. Film Score Composer: Crafted emotive music for feature films, aligning compositions with narratives for an immersive cinematic experience. National Cadet Corps 'A' Grade: Achieved 'A' grade certification, demonstrating discipline and leadership in training modules. Content Creator & Video Editor: Passionate food vlogger adept in Adobe Premiere Pro, creating captivating culinary exploration videos."
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
+        "description":"Professional Experience  ",
+        "name":"𝓜𝔂 𝓹𝓻𝓸𝓯𝓮𝓼𝓼𝓲𝓸𝓷𝓪𝓵 𝓮𝔁𝓹𝓮𝓻𝓲𝓮𝓷𝓬𝓮 𝓼𝓹𝓪𝓷𝓼 𝓻𝓸𝓵𝓮𝓼 𝓪𝓽 𝓐𝓬𝓬𝓮𝓷𝓽𝓾𝓻𝓮, 𝔀𝓱𝓮𝓻𝓮 𝓘 𝓵𝓮𝓿𝓮𝓻𝓪𝓰𝓮𝓭 𝓽𝓱𝓮 𝓜𝓲𝓬𝓻𝓸𝓼𝓸𝓯𝓽 𝓟𝓸𝔀𝓮𝓻 𝓟𝓵𝓪𝓽𝓯𝓸𝓻𝓶 𝓼𝓾𝓲𝓽𝓮 𝓽𝓸 𝓬𝓻𝓪𝓯𝓽 𝓲𝓷𝓷𝓸𝓿𝓪𝓽𝓲𝓿𝓮 𝔀𝓮𝓫 𝓪𝓷𝓭 𝓶𝓸𝓫𝓲𝓵𝓮 𝓪𝓹𝓹𝓵𝓲𝓬𝓪𝓽𝓲𝓸𝓷𝓼. 𝓘 𝓮𝔁𝓬𝓮𝓵𝓵𝓮𝓭 𝓲𝓷 𝓸𝓹𝓽𝓲𝓶𝓲𝔃𝓲𝓷𝓰 𝓾𝓼𝓮𝓻 𝓮𝔁𝓹𝓮𝓻𝓲𝓮𝓷𝓬𝓮𝓼 𝓽𝓱𝓻𝓸𝓾𝓰𝓱 𝓲𝓷𝓽𝓾𝓲𝓽𝓲𝓿𝓮 𝓲𝓷𝓽𝓮𝓻𝓯𝓪𝓬𝓮𝓼 𝓪𝓷𝓭 𝓻𝓮𝓪𝓵-𝓽𝓲𝓶𝓮 𝓼𝔂𝓼𝓽𝓮𝓶 𝓲𝓷𝓽𝓮𝓰𝓻𝓪𝓽𝓲𝓸𝓷𝓼. 𝓐𝓭𝓭𝓲𝓽𝓲𝓸𝓷𝓪𝓵𝓵𝔂, 𝓪𝓼 𝓪 𝓓𝓪𝓽𝓪 𝓐𝓷𝓪𝓵𝔂𝓼𝓽 𝓪𝓽 𝓔𝔁𝓬𝓮𝓵𝓮𝓻𝓪𝓽𝓮, 𝓘 𝓶𝓪𝓼𝓽𝓮𝓻𝓮𝓭 𝓽𝓱𝓮 𝓪𝓻𝓽 𝓸𝓯 𝓽𝓻𝓪𝓷𝓼𝓵𝓪𝓽𝓲𝓷𝓰 𝓬𝓸𝓶𝓹𝓵𝓮𝔁 𝓭𝓪𝓽𝓪𝓼𝓮𝓽𝓼 𝓲𝓷𝓽𝓸 𝓬𝓸𝓶𝓹𝓮𝓵𝓵𝓲𝓷𝓰 𝓿𝓲𝓼𝓾𝓪𝓵𝓼, 𝓮𝓶𝓹𝓸𝔀𝓮𝓻𝓲𝓷𝓰 𝓭𝓪𝓽𝓪-𝓭𝓻𝓲𝓿𝓮𝓷 𝓭𝓮𝓬𝓲𝓼𝓲𝓸𝓷-𝓶𝓪𝓴𝓲𝓷𝓰 𝓯𝓸𝓻 𝓫𝓾𝓼𝓲𝓷𝓮𝓼𝓼𝓮𝓼."
+      },
+      {
+        "description":"Skills  ",
+        "name":"𝓟𝓻𝓸𝓯𝓲𝓬𝓲𝓮𝓷𝓽 𝓲𝓷 𝓵𝓪𝓷𝓰𝓾𝓪𝓰𝓮𝓼 𝓼𝓾𝓬𝓱 𝓪𝓼 𝓟𝔂𝓽𝓱𝓸𝓷, 𝓙𝓪𝓿𝓪, 𝓪𝓷𝓭 𝓢𝓠𝓛, 𝓘 𝓪𝓶 𝔀𝓮𝓵𝓵-𝓿𝓮𝓻𝓼𝓮𝓭 𝓲𝓷 𝓪𝓷 𝓪𝓻𝓻𝓪𝔂 𝓸𝓯 𝓭𝓮𝓿𝓮𝓵𝓸𝓹𝓮𝓻 𝓽𝓸𝓸𝓵𝓼 𝓪𝓷𝓭 𝓽𝓮𝓬𝓱𝓷𝓸𝓵𝓸𝓰𝓲𝓮𝓼, 𝓲𝓷𝓬𝓵𝓾𝓭𝓲𝓷𝓰 𝓐𝓷𝓰𝓾𝓵𝓪𝓻, 𝓖𝓲𝓽𝓗𝓾𝓫, 𝓪𝓷𝓭 𝓣𝓮𝓷𝓼𝓸𝓻𝓕𝓵𝓸𝔀 𝓜𝔂 𝓪𝓬𝓪𝓭𝓮𝓶𝓲𝓬 𝓪𝓬𝓱𝓲𝓮𝓿𝓮𝓶𝓮𝓷𝓽𝓼 𝓮𝔁𝓽𝓮𝓷𝓭 𝓽𝓸 𝓹𝓾𝓫𝓵𝓲𝓬𝓪𝓽𝓲𝓸𝓷𝓼 𝓲𝓷 𝓮𝓼𝓽𝓮𝓮𝓶𝓮𝓭 𝓳𝓸𝓾𝓻𝓷𝓪𝓵𝓼, 𝓷𝓸𝓽𝓪𝓫𝓵𝔂 𝓻𝓮𝓬𝓸𝓰𝓷𝓲𝔃𝓮𝓭 𝓯𝓸𝓻 𝓶𝔂 𝔀𝓸𝓻𝓴 𝓸𝓷 𝓬𝔂𝓫𝓮𝓻𝓫𝓾𝓵𝓵𝔂𝓲𝓷𝓰 𝓭𝓮𝓽𝓮𝓬𝓽𝓲𝓸𝓷 𝓲𝓷 𝓼𝓸𝓬𝓲𝓪𝓵 𝓷𝓮𝓽𝔀𝓸𝓻𝓴𝓼."
+      },
+      {
+        "description":"ExtraCuricullar Activities   ",
+        "name":"𝓑𝓮𝔂𝓸𝓷𝓭 𝓶𝔂 𝓽𝓮𝓬𝓱𝓷𝓲𝓬𝓪𝓵 𝓹𝓻𝓸𝔀𝓮𝓼𝓼, 𝓘 𝓱𝓪𝓿𝓮 𝓭𝓮𝓶𝓸𝓷𝓼𝓽𝓻𝓪𝓽𝓮𝓭 𝓵𝓮𝓪𝓭𝓮𝓻𝓼𝓱𝓲𝓹 𝓪𝓼 𝓟𝓻𝓮𝓼𝓲𝓭𝓮𝓷𝓽 𝓸𝓯 𝓽𝓱𝓮 𝓘𝓣 𝓒𝓵𝓾𝓫, 𝓸𝓻𝓰𝓪𝓷𝓲𝔃𝓲𝓷𝓰 𝓽𝓮𝓬𝓱-𝓬𝓮𝓷𝓽𝓻𝓲𝓬 𝓮𝓿𝓮𝓷𝓽𝓼 𝓪𝓷𝓭 𝓯𝓸𝓼𝓽𝓮𝓻𝓲𝓷𝓰 𝓼𝓴𝓲𝓵𝓵 𝓭𝓮𝓿𝓮𝓵𝓸𝓹𝓶𝓮𝓷𝓽 𝓪𝓶𝓸𝓷𝓰 𝓶𝓮𝓶𝓫𝓮𝓻𝓼. 𝓐𝓭𝓭𝓲𝓽𝓲𝓸𝓷𝓪𝓵𝓵𝔂, 𝓶𝔂 𝓭𝓲𝓿𝓮𝓻𝓼𝓮 𝓲𝓷𝓽𝓮𝓻𝓮𝓼𝓽𝓼 𝓲𝓷𝓬𝓵𝓾𝓭𝓮 𝓬𝓸𝓶𝓹𝓸𝓼𝓲𝓷𝓰 𝓯𝓲𝓵𝓶 𝓼𝓬𝓸𝓻𝓮𝓼, 𝓪𝓬𝓱𝓲𝓮𝓿𝓲𝓷𝓰 '𝓐' 𝓰𝓻𝓪𝓭𝓮 𝓬𝓮𝓻𝓽𝓲𝓯𝓲𝓬𝓪𝓽𝓲𝓸𝓷 𝓲𝓷 𝓽𝓱𝓮 𝓝𝓪𝓽𝓲𝓸𝓷𝓪𝓵 𝓒𝓪𝓭𝓮𝓽 𝓒𝓸𝓻𝓹𝓼, 𝓪𝓷𝓭 𝓬𝓻𝓪𝓯𝓽𝓲𝓷𝓰 𝓮𝓷𝓰𝓪𝓰𝓲𝓷𝓰 𝓬𝓾𝓵𝓲𝓷𝓪𝓻𝔂 𝓬𝓸𝓷𝓽𝓮𝓷𝓽 𝓪𝓼 𝓪 𝓹𝓻𝓸𝓯𝓲𝓬𝓲𝓮𝓷𝓽 𝓿𝓲𝓭𝓮𝓸 𝓮𝓭𝓲𝓽𝓸𝓻 𝓪𝓷𝓭 𝓯𝓸𝓸𝓭 𝓿𝓵𝓸𝓰𝓰𝓮𝓻."
+      },
+      
     ]
   }
   
